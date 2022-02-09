5a705d3fde401af111980c82b6739be740c1d8d4
Adrian Ramirez <ramiire@gmail.com>
2022-02-09 11:37:08 +0100

    first commit, products and cart semi functional

---
+++ src/App.js
@@ -0,0 +1,80 @@
+import React, { useState, useEffect } from 'react';
+import { commerce } from './lib/commerce';
+import { Products, Navbar, Cart } from './components';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+const App = () => {
+    const [products, setProducts] = useState([]);
+    const [cart, setCart] = useState({});
+
+    const fetchProducts = async () => {
+        const { data } = await commerce.products.list();
+    
+        setProducts(data)
+    }
+
+    const fetchCart = async () => {
+        setCart(await commerce.cart.retrieve())
+    }
+
+    const handleAddToCart = async (productId, quantity) => {
+        const { cart }= await commerce.cart.add(productId, quantity);
+
+        setCart(cart);
+    }
+
+    const handleUpdateCartQty = async (productId, quantity) => {
+        const { cart } = await commerce.cart.update(productId, { quantity });
+
+        setCart(cart);
+    }
+
+    const handleRemoveFromCart = async (productId) => {
+        const { cart } = await commerce.cart.remove(productId);
+
+        setCart(cart);
+    }
+
+    const handleEmptyCart = async () => {
+        const { cart } = await commerce.cart.empty();
+
+        setCart(cart);
+    }
+
+    useEffect(() => {
+        fetchProducts();
+        fetchCart();
+    }, []);
+
+    console.log(cart)
+
+    return (
+        <Router>
+            <div>
+                <Navbar totalItems={cart.total_items} />
+                <Routes>
+                    <Route exact path="/" element={<Products products={products} onAddToCart={handleAddToCart} />} />
+                    <Route exact path="/cart" element={
+                        <Cart 
+                            cart={cart} 
+                            handleUpdateCartQty={handleUpdateCartQty}
+                            handleRemoveFromCart={handleRemoveFromCart}
+                            handleEmptyCart={ handleEmptyCart}
+                        />}
+                    />
+                </Routes>
+            </div>
+        </Router>
+
+    )
+}
+
+export default App
+
+
+
+
+// import Products from './components/Products/Products';
+// import Navbar from './components/Navbar/Navbar';
+// LO DE ARRIBA SIMPLIFICADO ABAJO
+// import { Products, Navbar } from './components'
