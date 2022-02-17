4fb374e794378a2e7b815a60327f7373e5c4be35
Adrian Ramirez <ramiire@gmail.com>
2022-02-17 13:13:35 +0100

    order sumary

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { commerce } from './lib/commerce';
-import { Products, Navbar, Cart } from './components';
+import { Products, Navbar, Cart, Checkout } from './components';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 const App = () => {
@@ -62,6 +62,7 @@ const App = () => {
                             handleEmptyCart={ handleEmptyCart}
                         />}
                     />
+                    <Route exact path="/checkout" element={<Checkout cart={cart} />} />
                 </Routes>
             </div>
         </Router>
---
+++ src/components/CheckoutForm/PaymentForm.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Typography, Button, Divider } from '@material-ui/core'
+import { Elements, CardElement, ElementsConsumer } from '@stripe/react-stripe-js';
+import { loadStrip } from '@stripe/stripe-js'
+import Review from './Review'
+
+const PaymentForm = ({ checkoutToken }) => {
+    return (
+        <>
+            <Review checkoutToken={checkoutToken} />
+        </>
+    )
+}
+
+export default PaymentForm
